79b0b594aea06f95fd18816a66569c302f9d2dfe
altdmb14 <altdmb14@gmail.com>
2020-06-03 12:46:32 +0500

    Adding static.js

---
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-layout row>
+        <v-text-field
+                label="New message"
+                placeholder="Write something"
+                v-model="text"
+                @keyup.enter="save"
+        />
+        <v-btn @click="save">
+            Save
+        </v-btn>
+    </v-layout>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+
+    export default {
+        props: ['messageAttr'],
+        data() {
+            return {
+                text:'',
+                id:null
+            }
+        },
+        watch: {
+            messageAttr: function(newVal, oldVal) {
+                this.text = newVal.text
+                this.is = newVal.id
+            }
+        },
+        methods: {
+            ...mapActions(['addMessageAction', 'updateMessageAction']),
+        save() {
+            const message = {
+                id: this.id,
+                text: this.text
+            }
+
+            if (this.id) {
+                this.updateMessageAction(message)
+            } else {
+                this.addMessageAction(message)
+            }
+            this.text = ''
+            this.id = null
+        }
+    }
+    }
+</script>
+
+<style>
+
+</style>
